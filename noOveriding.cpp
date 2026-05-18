e6f63bbdacc2d33afe2254dd8044a08dba59e91a
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-18 10:53:09 +0700

    menambahkan class derivedClass dan fungsi main untuk menampilkan perkenalan dari derived class

--- noOveriding.cpp
+++ noOveriding.cpp
@@ -7,3 +7,15 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
+class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Hallo saya Function dari derived class";
+    }
+};
+int main() {
+    derivedClass a;
+    a.perkenalan1();
+
+    return 0;
+}
